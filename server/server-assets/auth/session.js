3f9b847f23a31fffa0ca6033f74e0a42ddb7014c
k-cormack <k.cormack@gmail.com>
2018-09-04 14:42:01 -0600

    password update

--- server/server-assets/auth/session.js
+++ server/server-assets/auth/session.js
@@ -2,7 +2,7 @@ var expressSession = require("express-session");
 var mongoStore = require("connect-mongodb-session")(expressSession);
 
 var store = new mongoStore({
-  uri: "mongodb://kcormack:Abcd6789@ds014658.mlab.com:14658/summer18-kanban", //CHANGED!
+  uri: "mongodb://kcormack:abcd6789@ds014658.mlab.com:14658/summer18-kanban", //CHANGED!
   collection: "Sessions"
 });
 
@@ -12,7 +12,7 @@ store.on("error", function (err) {
 
 // @ts-ignore
 var session = expressSession({
-  secret: "you should change this", //CHANGE ME!!!!
+  secret: "changed!!!!", //CHANGE ME!!!!
   cookie: {
     maxAge: 1000 * 60 * 60 * 24 * 7 * 52 * 2,
   },
--- server/server-assets/db/mlab-config.js
+++ server/server-assets/db/mlab-config.js
@@ -1,5 +1,5 @@
 var mongoose = require('mongoose')
-var connectionString = 'mongodb://kcormack:Abcd6789@ds014658.mlab.com:14658/summer18-kanban' //Changed!
+var connectionString = 'mongodb://kcormack:abcd6789@ds014658.mlab.com:14658/summer18-kanban' //Changed!
 var connection = mongoose.connection
 
 
