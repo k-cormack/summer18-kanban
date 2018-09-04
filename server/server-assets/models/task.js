60882b80d3cee7bfdb838f61e915d8faed7d177c
k-cormack <k.cormack@gmail.com>
2018-09-04 14:24:54 -0600

    Models created

---
+++ server/server-assets/models/task.js
@@ -0,0 +1,21 @@
+let mongoose = require('mongoose')
+let Schema = mongoose.Schema
+let ObjectId = Schema.Types.ObjectId
+let schemaName = 'Task'
+
+let comment = new Schema({
+    description: {type: String, required: true},
+    created: { type: Number, required: true, default: Date.now() },
+    authorId: { type: ObjectId, ref: 'User', required: true }
+})
+
+let schema = new Schema({
+  title: { type: String, required: true },
+  description: { type: String, required: true },
+  created: { type: Number, required: true, default: Date.now() },
+  authorId: { type: ObjectId, ref: 'User', required: true },
+  comment: {comment}
+})
+
+
+module.exports = mongoose.model(schemaName, schema)
