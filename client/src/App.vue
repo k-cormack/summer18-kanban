e49238642121dcbddba959cbbe39580cb83ea48e
Rhianon Kilzer <rhianonkilzer@yahoo.com>
2018-09-10 10:58:28 -0600

    colors in app

--- client/src/App.vue
+++ client/src/App.vue
@@ -9,15 +9,21 @@
     display: flex;
     justify-content: center;
     font-family: 'Concert One', cursive;
-    /* font-family: "Avenir", Helvetica, Arial, sans-serif; */
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
     text-align: center;
     color: white;
-    background-image: linear-gradient(to top, #ffffff 0%, #087077 100%);
+    background-image: linear-gradient(to top, #ffffff 0%, #96dce6 100%);
     height: 700px;
     text-shadow: 2px 2px 3px black;
     padding: 0px
   }
-
 </style>
+
+
+<!-- 
+  lightest beige: #f0ece2
+2nd beige: #dfd3c3
+tan: #c7b198
+navy: #596e79 
+-->
